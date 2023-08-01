e3ddb0353681df440effa119a8aecd3088a62002
Rahul Parihar <rahul.parihar@jalantechnologies.com>
2023-08-01 12:51:26 +0530

    FORMIK AND YUP COMPLETE

---
+++ App/components/AppFormField.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppTextInput from './AppTextInput';
+import { ErrorMessage } from 'formik';
+function AppFormField({name}) {
+    return (
+        <>
+        </> 
+    );
+}
+
+export default AppFormField;
